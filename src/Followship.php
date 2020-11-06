ead0b9d969352b49f89c122bfd30c5a36f26851d
GuDzpoz <GuDzpoz@live.com>
2020-11-06 09:03:39 +0800

    Followship initial commit

---
+++ src/Followship.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace \Fedirum\Fedirum;
+
+use Flarum\Database\AbstractModel;
+
+class Followship extends AbstractModel {
+    protected $table = 'fedirum';
+    public $incrementing = false;
+
+    protected $attributes = [
+        'follower' => '',
+        'inbox' => ''
+    ];
+}

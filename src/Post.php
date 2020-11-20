07b1b79f9c8dbdb6f828380bf2ebce7738d3be19
GuDzpoz <GuDzpoz@live.com>
2020-11-20 21:10:03 +0800

    Listener to posting

--- src/Post.php
+++ src/Post.php
@@ -2,21 +2,54 @@
 
 namespace Fedirum\Fedirum;
 
-use Laminas\Diactoros\Response\HtmlResponse;
-use Laminas\Diactoros\Response\JsonResponse;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Component\Translation\TranslatorInterface;
 use Flarum\Post\Event\Posted;
+
+return [
+    
+];
 
 class Post
 {
-    protected $translator;
-    public function __construct(TranslatorInterface $translator) {
-        $this->translator = $translator;
+    public static function getPostLink($post) {
+        return Actor::getBaseLink() . '/d/' . $post->discussionId . '#' . $post->id;
+    }
+    
+    public function __construct() {
     }
 
     public function handle(Posted $event) {
+        $user = $event->actor;
+        $post = $event->post;
+        $contentSkim = '';
+        if($post->content) {
+            $contentSkim = substr($post->content, 0, 500);
+        }
+        $actorLink = Actor::getActorLink($user->username);
+        $content = [
+            '@context' => 'https://www.w3.org/ns/activitystreams',
+            'id' => self::getPostLink($post) . '-created',
+            'type' => 'Create',
+            'to' => [$actorLink . '/followers'],
+            'cc' => ['https://www.w3.org/ns/activitystreams#Public'],
+            'actor' => $actorLink,
+            'object' => [
+                'id' => self::getPostLink($post),
+                'url' => self::getPostLink($post),
+                'type' => 'Note',
+                'published' => gmdate('Y-m-d\TH:i:s\Z'),
+                'attributedTo' => $actorLink,
+                'content' => $contentSkim,
+                'to' => [$actorLink . '/followers'],
+                'cc' => ['https://www.w3.org/ns/activitystreams#Public']
+            ]
+        ];
+
+        $send = new Send();        
+        if($user) {
+            foreach(Followship::where('id', $user->id)->get() as $ship) {
+                $send->post($user->username, json_encode($content), $ship->inbox);
+            }
+        }
+
     }
 }
